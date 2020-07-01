4a23514d5de6aaa4b92f3948c548a3d17afbb707
aatauil <achrafatauil@hotmail.com>
2020-07-01 18:28:44 +0200

    move server config inside app file + basic pages & template

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const express = require("express");
-const db = require('./config/database')
-const server = require('./config/server');
-const path = require('path')
 const app = express();
+const db = require('./config/database')
+const path = require('path')
 
+// Server config
+
+app.set("port", process.env.PORT || 3000);
+app.listen(app.get("port"), () => {
+    console.log(
+        "App is running on http://localhost:%d in %s mode",
+        app.get("port"),
+        app.get("env")
+    )
+})
+
+// View engine setup
+app.set('views', path.join(__dirname, '/views'));
+console.log(__dirname)
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+
+// Static folder setup
 app.use(express.static(__dirname + '/public'));
 
 
-
-// Index
-
 app.get('/', (req, res) => {
-    res.render('index');
+    console.log("okkk")
+    // res.render('pages/index.ejs');
 });
 
 
