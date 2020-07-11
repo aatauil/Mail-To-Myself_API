a29baed223aa4bc62300809bfc85a8c7fd34511f
aatauil <achrafatauil@hotmail.com>
2020-07-11 14:11:09 +0200

    add file details inside request adapter

--- helpers/request-adapter.js
+++ helpers/request-adapter.js
@@ -1,11 +1,13 @@
 
-export default function RequestAdapter(request){
+export function RequestAdapter({request}){
     return (
         Object.freeze({
             path: request.path,
             method: request.method,
             text : request.body.text,
             email: request.body.email,
+            original: request.files[0].originalname,
+            modified: request.files[0].path
         })
     )
 }
